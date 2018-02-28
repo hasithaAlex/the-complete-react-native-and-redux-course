d24e7f0e31952f1fd3835dd183dc384a4738a183
hasitha madusanka <hasithamadusanka90_@outlook.com>
2018-02-28 18:22:31 +0530

    import component from others

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react'
+import { ReactNative, AppRegistry, Text } from 'react-native';
+import Header from './src/components/header';
+//Create a Component 
+const App = () => {
+    return (
+        <Header/>
+    );
+};
 
+
+//Render it to device
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//import libraries
+import  React  from 'react';
+import { Text } from 'react-native';
+
+//make components 
+const Header = () => {
+    return <Text>Albams</Text>;
+};
+
+//make the component avalible to other parts of the app 
+export default Header;
